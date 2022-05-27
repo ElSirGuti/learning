146560eec8b5c3d773e6e80c4ab2dc4963c6d4e8
ElSirGuti <87587118+ElSirGuti@users.noreply.github.com>
2022-05-27 18:16:09 -0400

    Add files via upload

---
+++ ddmmyy.py
@@ -0,0 +1,7 @@
+# Dates
+
+dd = (int(input("Enter the day you were born: ")))
+mm = (int(input("Enter the month you were born: ")))
+yy = (int(input("Enter the year you were born: ")))
+
+print(dd,mm,yy%100,sep='/')
